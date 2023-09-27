6cf01e7ce31de5dbc4ce8076341b0a8380cd9484
Kate test <d.swetlov2017@gmail.com>
2023-09-27 14:58:19 +0300

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    public static void main(String [] args) {
-       int currentAccount=100; //текущая сумма на счету
-        int replenishment=1200; //сумма пополнения
+    public static void main(String[] args) {
+        int currentAccount = 100; //текущая сумма на счету
+        int replenishment = 1200; //сумма пополнения
 
         int bonus;
-        if(replenishment>1000){
-            bonus = replenishment/100;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
         } else {
             bonus = 0;
         }
